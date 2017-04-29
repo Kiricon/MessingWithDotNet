112b7c678de520efcf732149c62a8817fe2f3b73
kiricon <kiricon@live.com>
2017-04-28 21:17:39 -0600

    Added .net files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MessingWithDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
